5dfa9bab47ed9841c221dcb6d8397e4a6f7a8647
Mihail Golubtsov <m.golubtsov@kryptonite.ru>
2022-03-10 21:47:48 +0300

    simple service with static model

--- src/main/scala/app/PredictorImpl.scala
+++ src/main/scala/app/PredictorImpl.scala
@@ -1,14 +1,26 @@
 package app
 
 
+//import java.util.concurrent.atomic.AtomicReference
 
 import test.model.{PredictRequest, PredictResponse, PredictorGrpc}
 
-import scala.concurrent.Future
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
 
 class PredictorImpl
-  extends PredictorGrpc.Predictor
-  {
+  extends PredictorGrpc.Predictor {
 
-  override def predict(request: PredictRequest): Future[PredictResponse] = Future.successful(PredictResponse(1))
+  private implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(4))
+
+  private val model = LogRegModel(Array(1, 2, 3, 4, 5), 10)
+
+  override def predict(request: PredictRequest): Future[PredictResponse] = Future {
+
+    PredictResponse(model.getScore(request.vector.toArray))
+  }
+}
+
+object PredictorImpl {
+
 }
