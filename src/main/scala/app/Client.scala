ddc20e74fd5622e03c888ed6d69a239f0909268d
Mihail Golubtsov <m.golubtsov@kryptonite.ru>
2022-03-10 22:29:49 +0300

    refactor & add client

--- src/main/scala/app/Client.scala
+++ src/main/scala/app/Client.scala
@@ -1,5 +1,40 @@
 package app
 
+import io.grpc.ManagedChannelBuilder
+import test.model.{PredictRequest, PredictorGrpc}
+
+import scala.util.Random
+
+
+/**
+ * client for imitating input stream of messages
+ */
 class Client {
 
+
+  private val rnd = new Random()
+
+  private def generateRequest(): PredictRequest = {
+    val vector = (for (_ <- 1 to 5) yield rnd.nextFloat()).toArray
+    val label = rnd.nextBoolean()
+    PredictRequest(vector, label)
+  }
+
+  def run(): Unit = {
+    val channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build
+    val blockingStub = PredictorGrpc.blockingStub(channel)
+
+
+    for (_ <- 1 to 1000) yield {
+      val result = blockingStub.predict(generateRequest())
+      println(result)
+      Thread.sleep(500)
+    }
+
+  }
+}
+
+
+object Client extends App {
+  new Client().run()
 }
--- src/main/scala/app/PredictorImpl.scala
+++ src/main/scala/app/PredictorImpl.scala
@@ -3,21 +3,31 @@ package app
 
 //import java.util.concurrent.atomic.AtomicReference
 
-import test.model.{PredictRequest, PredictResponse, PredictorGrpc}
+import test.model._
 
-import java.util.concurrent.Executors
+import java.util.logging.{Level, Logger}
 import scala.concurrent.{ExecutionContext, Future}
 
-class PredictorImpl
+class PredictorImpl(implicit executionContext: ExecutionContext)
   extends PredictorGrpc.Predictor {
-
-  private implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(4))
 
   private val model = LogRegModel(Array(1, 2, 3, 4, 5), 10)
 
+  private val logger = Logger.getLogger(classOf[PredictorService].getName)
+
+
+
+  // CHALLENGE NOTE: I decided to use here simple request-response api, but
+  // streaming api with observer can also be used
   override def predict(request: PredictRequest): Future[PredictResponse] = Future {
 
+    logger.log(Level.INFO, s"request with ${request.vector.mkString("[", ", ", "]")}")
+
     PredictResponse(model.getScore(request.vector.toArray))
+  }
+
+  override def changeModel(request: ChangeModelRequest): Future[ChangeModelResponse] = Future {
+    ChangeModelResponse("not implemented")
   }
 }
 
