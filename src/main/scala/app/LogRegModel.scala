ae33d1750688597ac95ed5ebcecca359747ff9e9
Mihail Golubtsov <m.golubtsov@kryptonite.ru>
2022-03-11 12:43:29 +0300

    refactor & scaladoc

--- src/main/scala/app/LogRegModel.scala
+++ src/main/scala/app/LogRegModel.scala
@@ -7,7 +7,7 @@ case class LogRegModel(weights: Vector[Float], bias: Float) {
   /**
    * scalar product of two vectors
    * matching length ensured by calling function
-   *
+   * CHALLENGE NOTE: math libraries can be used here instead of collection transformations
    * @param left  - left operand of product
    * @param right - right operand of product
    * @return scalar product
@@ -16,10 +16,23 @@ case class LogRegModel(weights: Vector[Float], bias: Float) {
     left.zip(right).map { case (leftElement, rightElement) => leftElement * rightElement }.sum
   }
 
+  /**
+   * fit value in (0, 1) range
+   * @param value
+   * @return
+   */
   private def normalizeValue(value: Float): Float = (1 / (1 + exp(-value))).toFloat
 
   override def toString: String = s"weights:${weights.mkString("[", ",", "]")}, bias: $bias"
 
+  /**
+   * Get score of logistic regression model.
+   * if weights are w1, w2, w3,... and bias is b, and sample is vector with  components x1, x2, x3, ...
+   * then z = b + x1 * w1 + x2 * w2 + ... + xn * wn
+   * score = 1 / (1 + exp(-z))
+   * @param sample
+   * @return score, float ranged from 0 to 1
+   */
   def getScore(sample: Vector[Float]): Float = {
     require(sample.length == weights.length, "Sample and weights size not match")
 
--- src/test/scala/app/LogRegModelSpec.scala
+++ src/test/scala/app/LogRegModelSpec.scala
@@ -6,16 +6,16 @@ import org.scalatest.matchers.should.Matchers
 
 class LogRegModelSpec extends AnyFlatSpec with Matchers {
 
-  private val testModel = LogRegModel(Array(1F, 1F, 1F), 0)
+  private val testModel = LogRegModel(Vector(1F, 1F, 1F), 0)
 
   it should "calculate score" in {
-    val testVector = Array(1F, 1F, 1F)
+    val testVector = Vector(1F, 1F, 1F)
     assert(testModel.getScore(testVector) - 0.95257413 < 0.0001)
 
   }
 
   it should "throw exception if size not match" in {
-    val testVector = Array(3F)
+    val testVector = Vector(3F)
 
     the[IllegalArgumentException] thrownBy {
       testModel.getScore(testVector)
