9fbb97e3f4073157a54b9e166015ddc00e3cc715
Mikhail Golubtsov <mp.golubtcov@physics.msu.ru>
2023-11-10 10:12:22 +0200

    fix entrypoint

--- typelevel-service/src/main/scala/app/PredictorApp.scala
+++ typelevel-service/src/main/scala/app/PredictorApp.scala
@@ -2,10 +2,8 @@ package app
 
 import cats.effect.{IO, IOApp}
 
-import java.lang.Exception
-
 object PredictorApp extends IOApp.Simple {
 
-  override val run: IO[Unit] = PredictorService.run[IO].handleError(err => err.getMessage)
+  override val run: IO[Unit] = PredictorService.run[IO]
 
 }
